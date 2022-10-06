fd532b0316b7c2694a6c854e1dde731dc013b6e6
Rishi Collins <67651571+collinsrs@users.noreply.github.com>
2022-10-06 09:33:07 -0400

    Added CORS support

--- lib/express.ts
+++ lib/express.ts
@@ -1,3 +1,16 @@
 import Express from 'express';
+import cors from 'cors';
+
+const corsOrigins = {
+    origin: [
+        'https://core.innerva.io',
+        'https://pomfretcard.com',
+        'http://localhost:3000',
+        'http://localhost:9999',
+    ],
+};
+
+
 const api = Express();
+api.use(cors(corsOrigins));
 export default api;
